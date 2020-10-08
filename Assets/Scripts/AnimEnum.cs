fa3786c79569441bcf9486654700165e32906b9a
HW <haowenjhang@gmail.com>
2020-10-08 18:43:01 +0800

    Initial commit

---
+++ Assets/Scripts/AnimEnum.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimEnum : MonoBehaviour
+{
+    public static string AnimatorState = "State";
+
+    public static string BlendTree = "Speed";
+
+
+    public enum StateType : int
+    {  
+        Idel = 1,
+        Pose = 2,
+        Jump = 3,
+    }
+}
---
+++ Assets/Scripts/AnimcontrolManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimcontrolManager : MonoBehaviour
+{
+
+    Animator ModelAnimator;
+
+    private float speed = 100;
+
+
+    void Start()
+    {
+        ModelAnimator = this.GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+
+        ModelAnimator.SetFloat(AnimEnum.BlendTree, Input.GetAxis("Vertical") * speed);
+
+
+        if (Input.GetKeyUp(KeyCode.E))
+            ChangeState(AnimEnum.StateType.Jump);
+
+
+        else if (Input.GetKeyUp(KeyCode.Q))
+            ChangeState(AnimEnum.StateType.Pose);
+
+
+        else
+            ChangeState(AnimEnum.StateType.Idel);
+    }
+
+
+    void ChangeState(AnimEnum.StateType StateChangeType)
+    {
+
+        if (ModelAnimator == null)
+            return;
+
+        ModelAnimator.SetInteger(AnimEnum.AnimatorState, (int)StateChangeType);
+    }
+
+
+
+}
